d09ad5187566a3920be11746ab72ad8a19c471ce
Jaroslove <jroslovemonro@gmail.com>
2017-02-08 23:10:10 +0300

    Initial commit

---
+++ src/main/ru.diplom.server/MyServlet.java
@@ -0,0 +1,16 @@
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+/**
+ * Created by 1 on 08.02.2017.
+ */
+@WebServlet
+public class MyServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
