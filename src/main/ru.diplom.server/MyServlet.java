1b54dbd7e2b2ccf83773c55c21e8e945d5a89271
Jaroslove <jroslovemonro@gmail.com>
2017-02-08 23:21:15 +0300

    Add Annotation WebServlet

--- src/main/ru.diplom.server/MyServlet.java
+++ src/main/ru.diplom.server/MyServlet.java
@@ -1,16 +1,20 @@
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by 1 on 08.02.2017.
  */
-@WebServlet
+@WebServlet(name = "DiplomServlet", urlPatterns = {"/a/b/c","/main/diplom"})
 public class MyServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>"+"25"+"</h1>");
+        out.flush();
     }
 }
